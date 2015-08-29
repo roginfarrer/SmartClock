6911663963dadae5c34a341b0ad90842d4eed399
roginfarrer <rogin@roginfarrer.com>
2015-08-29 00:36:29 -0400

    Groovylock support

---
+++ Configs.js
@@ -0,0 +1,4 @@
+var vertical_spacing = 200;
+var notification_spacing = 55;
+var xhour = true; // Enable for 12h clock. Disable for 24h.
+var green = true; // Activate for green battery charging indicator.
